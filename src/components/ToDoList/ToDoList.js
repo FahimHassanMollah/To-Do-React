aef69abc84e6cc41b3d5d2bd4faad9a2df5a5de3
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-09-24 10:47:07 +0600

    todo app created with delete todo  functionalities

---
+++ src/components/ToDoList/ToDoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Col, ListGroupItem, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+const ToDoList = (props) => {
+    return (
+        <div className="">
+
+            <Row className="pt-2">
+                <Col md={2} xs={2}>
+                    <Button onClick={()=>props.deleteListHandler(props.listItem)} variant={props.color} className="py-2 px-2"> <span> <FontAwesomeIcon size="lg" icon={faWindowClose} /></span></Button>
+                </Col>
+                <Col md={10} xs={10}>
+                    <ListGroupItem className="py-2 px-2" variant={props.color}>
+                        {props.list}
+                    </ListGroupItem>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default ToDoList;
