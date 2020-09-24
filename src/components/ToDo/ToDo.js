587d8f572e38591436b5d6d440ddae9be30eb3fb
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-09-24 10:53:14 +0600

    remove empty lines

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -3,31 +3,25 @@ import { Button, Col, Container, FormControl, InputGroup, ListGroup, Row } from 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faPlus } from '@fortawesome/free-solid-svg-icons'
 import ToDoList from '../ToDoList/ToDoList';
-
 const ToDo = () => {
     const [item, setItem] = useState("")
     const [itemArray, setitemArray] = useState([])
     const [checker, setChecker] = useState(false)
     const colorArray = ["success", "dark", "info", "primary", "success", "dark", "info", "primary", "success", "dark", "info", "primary", "success", "dark", "info", "primary", "success", "dark", "info", "primary", "success", "dark", "info", "primary", "success", "dark", "info", "primary"]
     const inputItemHandler = (e) => {
-
         setItem(e.target.value)
         let value = e.target.value;
-
     }
     const addItemHandler = () => {
         setitemArray([...itemArray, item])
         setItem("")
         // e.target.value=""
         // setItem("")
-
-
     }
     const deleteListHandler = (list) => {
-       let lists=[...itemArray]
-       lists.splice(list,1)
-       setitemArray(lists)
-
+        let lists = [...itemArray]
+        lists.splice(list, 1)
+        setitemArray(lists)
     }
     return (
         <div>
@@ -43,7 +37,6 @@ const ToDo = () => {
                     <Row className="justify-content-md-center">
                         <Col md={4} >
                             <InputGroup size="md">
-
                                 <FormControl value={item} onChange={inputItemHandler} placeholder="add a item" />
                                 <div className="px-4">
                                     <Button onClick={addItemHandler} variant="outline-success">  <FontAwesomeIcon size="lg" icon={faPlus} /></Button>
@@ -53,14 +46,11 @@ const ToDo = () => {
                     </Row>
                     <Row className="justify-content-md-center pt-4">
                         <Col md={4} >
-
                             <ListGroup>
                                 {
                                     itemArray.map((list, index) => <ToDoList key={index} listItem={index} deleteListHandler={deleteListHandler} color={colorArray[index]} list={list}></ToDoList>)
                                 }
                             </ListGroup>
-
-
                         </Col>
                     </Row>
                 </Container>
@@ -68,5 +58,4 @@ const ToDo = () => {
         </div>
     );
 };
-
 export default ToDo;
--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Button, Col, ListGroupItem, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faWindowClose } from '@fortawesome/free-solid-svg-icons'
-
 const ToDoList = (props) => {
     return (
         <div className="">
-
             <Row className="pt-2">
                 <Col md={2} xs={2}>
                     <Button onClick={()=>props.deleteListHandler(props.listItem)} variant={props.color} className="py-2 px-2"> <span> <FontAwesomeIcon size="lg" icon={faWindowClose} /></span></Button>
@@ -20,5 +18,4 @@ const ToDoList = (props) => {
         </div>
     );
 };
-
 export default ToDoList;
